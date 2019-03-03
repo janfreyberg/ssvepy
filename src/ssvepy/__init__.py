1d5defcd625b1d5ea885f9c212473b3bf9c5058a
Jan Freyberg <jan.freyberg@gmail.com>
2019-03-03 16:40:41 +0000

    Correctly get version of package in init file

--- src/ssvepy/__init__.py
+++ src/ssvepy/__init__.py
@@ -3,4 +3,4 @@ import pkg_resources
 from .ssvepyepochs import Ssvep, load_ssvep
 from . import frequencymaths
 
-__version__ = "0.0.1"
+__version__ = pkg_resources.get_distribution("ssvepy").version
